6f9ade08063d4be9d2e2965a2e98d6b3b9a33c84
Vlakar56 <vlakar56@yandex.ru>
2024-08-17 17:57:01 +0000

    Add files via upload
    Цикл While

---
+++ 2.3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    numbers = my_list[index]
+    index += 1
+    if numbers <= 0:
+        continue
+    print(numbers)
+
+
+
+
