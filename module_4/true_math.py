12f95a962332cf0d3e95d6f71f635c7020c8b01e
Vlakar56 <vlakar56@yandex.ru>
2024-09-07 16:43:33 +0500

    true_math.py

---
+++ module_4/true_math.py
@@ -0,0 +1,10 @@
+rom math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
+# print(divide(49, 7))
+# print(divide(15, 0))
