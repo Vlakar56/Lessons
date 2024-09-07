d899786c79649c48e80e38ae68d4b4c1983e1eda
Vlakar56 <vlakar56@yandex.ru>
2024-09-07 16:42:52 +0500

    module_4.1.py

---
+++ module_4/module_4.1.py
@@ -0,0 +1,14 @@
+import true_math as true
+import fake_math as fake
+
+true_divide = true.divide
+fake_divide = fake.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
